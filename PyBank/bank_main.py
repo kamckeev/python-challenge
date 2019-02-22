a0501c00a09365125687bea90163acacc0ec13cc
Kim.McK <k.mckeever@alumni.unc.edu>
2019-02-22 00:06:37 -0500

    started bank_main

--- PyBank/bank_main.py
+++ PyBank/bank_main.py
@@ -1,6 +1,23 @@
 import os
 import csv
-csvpath = os.path.join('..', 'PyBoss', 'budget_data.csv')
+csvpath = os.path.join('..', 'PyBank', 'budget_data.csv')
 with open(csvpath, newline= '') as csvfile:
     csvreader=csv.reader(csvfile, delimiter=',')
     next(csvreader, None)
+    csv_header = next(csvfile)
+   
+
+#In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. 
+#You will give a set of financial data called budget_data.csv. The dataset is composed of two columns: 
+# Date and Profit/Losses. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
+#Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+#The total number of months included in the dataset
+
+#The net total amount of "Profit/Losses" over the entire period
+def net_volume()
+#The average of the changes in "Profit/Losses" over the entire period
+
+#The greatest increase in profits (date and amount) over the entire period
+
+#The greatest decrease in losses (date and amount) over the entire period
