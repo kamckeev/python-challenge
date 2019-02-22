c8aafd69b98e636eb4152e9c7851a8328481916c
Kim.McK <k.mckeever@alumni.unc.edu>
2019-02-21 23:35:53 -0500

    update poll_main.py

--- PyPoll/poll_main.py
+++ PyPoll/poll_main.py
@@ -4,13 +4,8 @@ csvpath = os.path.join('..', 'PyPoll', 'election_data2.csv')
 with open(csvpath, newline= '') as csvfile:
     csvreader=csv.reader(csvfile, delimiter=',')
     
-   
-     # number of votes
     csv_header = next(csvfile)
-    
-    # A complete list of candidates who received votes
-    # A complete list of candidates who received votes
-    
+         
     candidates= []
     counties = []
     voter_ID= []
@@ -19,29 +14,44 @@ with open(csvpath, newline= '') as csvfile:
         counties.append(i[1])
         voter_ID.append(i[0])
     row_count = len(voter_ID)
-    candidate_list= (set(candidates))
-   
     
     # A complete list of candidates who received votes
-    
+    candidate_list= (set(candidates))
+    print(candidate_list)
+    Khan= [i for i in candidates if i == 'Khan']
+    Khan_votes=(len(Khan))
+    print(f'Khan: {Khan_votes}')
+
+    Li=[i for i in candidates if i == 'Li']
+    Li_votes=(len(Li))
+    print(f'Li: {Li_votes}')
+
+    Correy=[i for i in candidates if i == 'Correy']
+    Correy_votes=(len(Correy))
+    print(f'Correy: {Correy_votes}')
+
+    OTooley= [i for i in candidates if i == "O'Tooley"]
+    OTooley_votes=(len(OTooley))
+    print(f"O'Tooley: {OTooley_votes}")
 
 
-    
+
+   
            
    
    
 
     #printout:
-    # Election Results
-    # *** 
-    #print ('Total number of votes cast:')
-    
-    #print(f'The total amount of votes cast this election was {row_count} votes!')
+    #print('{:*^50}'.format('Election Results'))
+    #print()
+    # number of votes
+    #print('{:^30}'.format(f'The total number of votes cast was {row_count} votes!'))
+    #print('{:^50}'.format('~'))
+
     # ***
-    # Corey, Khan, Li, O'Tuley canidates, list them in order of percentages of votes received
+    # The percentage of votes each candidate won
     # ***
-    #print('Our candidates!:')
-   # print(list(candidate_list))
+    #The winner of the election based on popular vote
     # the winner: name
     # ***
     # some cool asci picture maybe? Related to voting
