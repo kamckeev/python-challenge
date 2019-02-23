86b4fd246c3c60e4a0370d71e3236a7eb8081bdf
Kim.McK <k.mckeever@alumni.unc.edu>
2019-02-23 16:39:51 -0500

    testing enviroment for polling data

--- PyPoll/poll_test.py
+++ PyPoll/poll_test.py
@@ -1,6 +1,6 @@
 import os
 import csv
-canidates_dict ={}
+candidate_raws ={}
 candidates= []
 csvpath = os.path.join('..', 'PyPoll', 'election_data2.csv')
 with open(csvpath, newline= '') as csvfile:
@@ -10,25 +10,34 @@ with open(csvpath, newline= '') as csvfile:
          
     for i in csvreader:
         candidates.append(i[2])
-        if i[2] not in canidates_dict:
-            canidates_dict[i[2]]=1
+        if i[2] not in candidate_raws:
+            candidate_raws[i[2]]=1
         else:
-            canidates_dict[i[2]] = canidates_dict[i[2]]+1
+            candidate_raws[i[2]] = candidate_raws[i[2]]+1
     row_count = len(candidates)
-    
+candidate_names =list(candidate_raws)
+candidate_votes= list(candidate_raws.values())
+winner_value= max(candidate_votes)
 
-    #printout:
-    print('{:*^50}'.format('Election Results'))
-    print('{:^30}'.format(f'The total number of votes cast was {row_count} votes!'))
-    print('{:^50}'.format('~'))
-    # The percentage of votes each candidate won
-    print(f'Candidate () had ()% of the vote, (x votes)')
-    print(f'Candidate () had ()% of the vote, (x votes)')
-    print(f'Candidate () had ()% of the vote, (x votes)')
-    print(f'Candidate () had ()% of the vote, (x votes)')
-    print('{:^50}'.format('~'))
-   #The winner of the election based on popular vote
-    # the winner: name
-    print(f'The winner was: (function needed to search for greatest numbrr of votes')
-    print('{:*^50}'.format(''))
+winner_name = 'n/a'
+for name, votes in candidate_raws.items(): 
+    if votes == winner_value:
+        winner_name= name 
+
+
+
+#printout:
+print('{:*^50}'.format('Election Results'))
+print('{:^30}'.format(f'The total number of votes cast was {row_count} votes!'))
+print('{:^50}'.format('~'))
+# The percentage of votes each candidate won
+print(f'Candidate () had ()% of the vote, (x votes)')
+print(f'Candidate () had ()% of the vote, (x votes)')
+print(f'Candidate () had ()% of the vote, (x votes)')
+print(f'Candidate () had ()% of the vote, (x votes)')
+print('{:^50}'.format('~'))
+#The winner of the election based on popular vote
+# the winner: name
+print(f'The winner was: {winner_name}')
+print('{:*^50}'.format(''))
 
