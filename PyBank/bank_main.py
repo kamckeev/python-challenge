88923b8592bc345263fcdf2b7b68aa519acd4a7a
Kim.McK <k.mckeever@alumni.unc.edu>
2019-02-22 22:24:10 -0500

    saving updates

--- PyBank/bank_main.py
+++ PyBank/bank_main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import numpy as np
 csvpath = os.path.join('..', 'PyBank', 'budget_data.csv')
 with open(csvpath, newline= '') as csvfile:
     csvreader=csv.reader(csvfile, delimiter=',')
@@ -17,17 +18,38 @@ with open(csvpath, newline= '') as csvfile:
     profit=[]
     for i in csvreader:
         months.append(i[0])
-        profit.append(i[1])
+        profit.append(int(i[1]))
     #print(len(months))
- 
+    #print(profit)
 #The net total amount of "Profit/Losses" over the entire period
-    def net_profit()
-        (i for i in list(profit))/2
-    print()
-#def net_volume()
 
-#The average of the changes in "Profit/Losses" over the entire period
+#profit_sum = 0
+#for i in profit:
+#    profit_sum += i
+#print(profit_sum)
+
+def list_sum(input_list):
+    total = 0
+    for i in input_list:
+        total += i
+    return(total)
+
+print(f'This is the total profit: ${(list_sum(profit))}')
+
+change=[]
+for i in range(len(profit)-1):
+    this_change = profit[i+1]-profit[i]
+    change.append(this_change)
+    
+avg_change = list_sum(change)/len(change)
+
+#reducing avg_change to two decimal points to represent money
+print(round(avg_change,2))
 
 #The greatest increase in profits (date and amount) over the entire period
+max_profit= max(change)
+print(f'Maximum profit: ${max_profit}')
 
 #The greatest decrease in losses (date and amount) over the entire period
+min_profit= min(change)
+print(f'Minimum profit: ${min_profit}')
